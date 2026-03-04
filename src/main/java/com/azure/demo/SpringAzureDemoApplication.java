16f9c6b04c7249344c1b722324e785503d87a3b6
niranjanw89 <niranjan.wad@gmail.com>
2026-03-03 23:26:40 -0500

    Updating dependencies

--- src/main/java/com/azure/demo/SpringAzureDemoApplication.java
+++ src/main/java/com/azure/demo/SpringAzureDemoApplication.java
@@ -2,11 +2,11 @@ package com.azure.demo;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootConfiguration
+@SpringBootApplication
 @RestController
 public class SpringAzureDemoApplication {
 
